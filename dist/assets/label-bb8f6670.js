8c16569dd71ba308e4e25319d947a4d9428b1c80
mumaggos <63526461+mumaggos@users.noreply.github.com>
2025-06-15 19:32:10 +0100

    Add files via upload

---
+++ dist/assets/label-bb8f6670.js
@@ -0,0 +1,6 @@
+import{an as i,aj as d,aq as c}from"./index-21c6ac15.js";import{j as r,P as m}from"./ui-9609bd02.js";import{r as s}from"./vendor-48cd178f.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=i("Save",[["path",{d:"M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z",key:"1owoqh"}],["polyline",{points:"17 21 17 13 7 13 7 21",key:"1md35c"}],["polyline",{points:"7 3 7 8 15 8",key:"8nz8an"}]]);var f="Label",l=s.forwardRef((e,t)=>r.jsx(m.label,{...e,ref:t,onMouseDown:a=>{var o;a.target.closest("button, input, select, textarea")||((o=e.onMouseDown)==null||o.call(e,a),!a.defaultPrevented&&a.detail>1&&a.preventDefault())}}));l.displayName=f;var n=l;const u=c("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=s.forwardRef(({className:e,...t},a)=>r.jsx(n,{ref:a,className:d(u(),e),...t}));p.displayName=n.displayName;export{p as L,w as S};
---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          web3: ['ethers', 'wagmi', 'viem'],
+          ui: ['@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu', '@radix-ui/react-tabs'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
+})
+
